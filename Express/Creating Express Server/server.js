8240b6d6b3ef07dd3dd0a46d8f76274a506d6b84
azim-at <azeem.talikoti@gmail.com>
2022-12-08 21:28:42 +0530

    Server created

--- Express/Creating Express Server/server.js
+++ Express/Creating Express Server/server.js
@@ -1,4 +1,6 @@
 // Creating express server
 const express = require("express");
 const app = express();
-app.listen(3000);
+app.listen(3000, function() {
+    console.log("Server started at port 3000");
+});
